f4dc49af6e3f73ff93b5ff6387e672c62101a27f
Joseph Kinahan <kinahanj25@gmail.com>
2024-04-11 22:37:38 +0100

    added new Food Catalogue MS and working ep, and one calling the restaurant listing MS and DB using Eureka

---
+++ foodcatalogue/src/main/java/com/majortomdev/foodcatalogue/mapper/FoodItemMapper.java
@@ -0,0 +1,18 @@
+package com.majortomdev.foodcatalogue.mapper;
+
+import com.majortomdev.foodcatalogue.dto.FoodItemDTO;
+import com.majortomdev.foodcatalogue.entity.FoodItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface FoodItemMapper {
+
+    FoodItemMapper INSTANCE = Mappers.getMapper(FoodItemMapper.class);
+
+    FoodItem mapFoodItemDTOToFoodItem(FoodItemDTO foodItemDTO);
+
+    FoodItemDTO mapFoodItemToFoodItemDto(FoodItem foodItem);
+
+
+}
---
+++ foodcatalogue/src/test/java/com/majortomdev/foodcatalogue/FoodCatalogueMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majortomdev.foodcatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCatalogueMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
